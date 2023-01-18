bff57495a81f487914cc8753ee5fbceebb4e1892
hg68 <65812087+hg68@users.noreply.github.com>
2023-01-18 16:42:58 +1030

    client done. h file in progress

--- minitalk.h
+++ minitalk.h
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hgill <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,44 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#ifndef MINITALK_H
+# define MINITALK_H
 
-void	ft_send_bits(int pid, char i)
-{
-	int	bit;
+# include "../libft/inc/libft.h"
+# include "../libft/inc/ft_printf.h"
+# include <signal.h>
 
-	bit = 0;
-	while (bit < 8)
-	{
-		if ((i & (0x01 << bit)) != 0)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		usleep(300);
-		bit++;
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	int	pid;
-	int	i;
-
-	i = 0;
-	if (argc == 3)
-	{
-		pid = ft_atoi(argv[1]);
-		while (argv[2][i] != '\0')
-		{
-			ft_send_bits(pid, argv[2][i]);
-			i++;
-		}
-		ft_send_bits(pid, '\n');
-	}
-	else
-	{
-		ft_printf("\033Error.\033[0m\n");
-		return (1);
-	}
-	return (0);
-}
+#endif
