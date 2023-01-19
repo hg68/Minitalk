f19ee02d3da0a84b66420568d6479e6db3537057
Hargun Gill <hgill@F2N016.42adel.org.au>
2023-01-19 12:42:11 +1030

    minitalk.h updated file path

--- minitalk.h
+++ minitalk.h
@@ -6,15 +6,15 @@
 /*   By: hgill <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 15:27:52 by hgill             #+#    #+#             */
-/*   Updated: 2023/01/04 18:03:58 by hgill            ###   ########.fr       */
+/*   Updated: 2023/01/19 12:40:05 by hgill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "../libft/inc/libft.h"
-# include "../libft/inc/ft_printf.h"
+# include "../libft/libft/libft.h"
+# include "../libft/ft_printf/ft_printf.h"
 # include <signal.h>
 
 #endif
