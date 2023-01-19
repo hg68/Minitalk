3c5aac4d5461f321be64006223595563dbf823e7
Hargun Gill <hgill@F2N016.42adel.org.au>
2023-01-19 12:36:15 +1030

    adding libft_all

---
+++ libft/ft_printf/ft_printf.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgill <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 16:40:44 by hgill             #+#    #+#             */
+/*   Updated: 2022/06/17 16:49:39 by hgill            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	ft_check_format(char c, va_list *args, int *l, int *i)
+{
+	if (c == 's')
+		ft_string(va_arg(*args, char *), l);
+	else if (c == 'd' || c == 'i')
+		ft_number(va_arg(*args, int), l);
+	else if (c == 'u')
+		ft_unsigned_int(va_arg(*args, unsigned int), l);
+	else if (c == 'x')
+		ft_hexadecimal(va_arg(*args, unsigned int), l, 'x');
+	else if (c == 'X')
+		ft_hexadecimal(va_arg(*args, unsigned int), l, 'X');
+	else if (c == 'p')
+		ft_pointer(va_arg(*args, size_t), l);
+	else if (c == 'c')
+		ft_putchar_length(va_arg(*args, int), l);
+	else if (c == '%')
+		ft_putchar_length('%', l);
+	else
+		(*i)--;
+}
+
+int	ft_printf(const char *string, ...)
+{
+	va_list	args;
+	int		i;
+	int		l;
+
+	i = 0;
+	l = 0;
+	va_start(args, string);
+	while (string[i] != '\0')
+	{
+		if (string[i] == '%')
+		{
+			i++;
+			ft_check_format(string[i], &args, &l, &i);
+			i++;
+		}
+		else
+		{
+			ft_putchar_length((char)string[i], &l);
+			i++;
+		}
+	}
+	va_end(args);
+	return (l);
+}
